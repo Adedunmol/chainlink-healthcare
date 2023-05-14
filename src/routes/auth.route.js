795428de2cdf50981207a05fecc46118ace51d74
Adedunmola <oyewaleadedunmola@gmail.com>
2023-05-14 16:21:05 +0100

    fix bug in auth.route

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,9 +1,9 @@
 const express = require('express')
-const { registerUserController, loginUserController } = require('../controllers/auth.controller')
+const { registerUserController, loginUserController, logoutUserController } = require('../controllers/auth.controller')
 const router = express.Router()
 
 router.route('/register').post(registerUserController)
 router.route('/login').post(loginUserController)
-router.route('/logout').get(logout)
+router.route('/logout').get(logoutUserController)
 
 module.exports = router
